1b51948b9c25b5078136af7040c501cc72827b50
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2021-01-05 20:25:41 +0000

    Add Get Projects

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,20 @@
+import {GET_PROJECTS} from "../actions/types";
+
+const initialState = {
+    projects:[],
+    project:{}
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case GET_PROJECTS:
+            return {
+                ...state,
+                projects:action.payload
+            }
+
+        default:
+            return state;
+    }
+}
