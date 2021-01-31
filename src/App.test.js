5001c9bb2f76c1cfaf59f66da3ec6324f620ae12
Anderson de Brito Oliveira <andersonbritooliveiradev@gmail.com>
2021-01-31 19:56:53 +0000

    Bug Fixes

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Projects', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Dashboard/i);
-  console.log(linkElement);
-  expect(linkElement).toBeInTheDocument();
-});
