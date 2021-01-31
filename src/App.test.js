fde7cf83772534c089d6244891960c52a9a8d66d
Anderson de Brito Oliveira <andersonbritooliveiradev@gmail.com>
2021-01-31 19:26:20 +0000

    SecuredRoutes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Welcome to the Dashboard', () => {
+test('Projects', () => {
   render(<App />);
   const linkElement = screen.getByText(/Dashboard/i);
   console.log(linkElement);
